fafa9f99f028c00b4d045522d4fdadbbed0da9a2
Ramonprog <programador.ramon@gmail.com>
2024-03-15 19:58:19 -0300

    instalando e configurando router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Login } from './pages/Login/index.tsx'
 import './globals.css'
+import { BrowserRouter } from 'react-router-dom'
+import { MainRoutes } from './routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <MainRoutes />
+  </BrowserRouter>
 )
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,17 @@
+import { FormEvent } from 'react'
 import styles from './styles.module.scss'
+import { useNavigate } from 'react-router-dom'
 export function Login() {
+    const navigate = useNavigate()
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault()
+        navigate('/dashboard')
+    }
+
     return (
         <div className={styles.container}>
             <h2>Sign In</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="email" placeholder="Insira seu e-mail" />
                 <input type="text" placeholder="Insira Sua senha" />
                 <button type="submit">Login</button>
