91b366d1cd6fe361292caa641f3884d4341bc9c0
Ramonprog <programador.ramon@gmail.com>
2024-03-17 11:54:02 -0300

    testando pagina de pokemons details

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -2,6 +2,47 @@ describe("Testa o dashboard", () => {
   it("Deve carregar uma lista com 4 pokemons", () => {
     cy.visit("/dashboard");
 
-    cy;
+    cy.intercept("GET", "http://localhost:3000/pokemon", {
+      fixture: "pokemons.json",
+    });
+
+    cy.contains("Pikachu");
+    cy.contains("Rotom");
+    cy.contains("Charmander");
+    cy.contains("Geodude");
+  });
+  it("Ao clicar em um pokemon, deve abrir a página de detalhes", () => {
+    cy.visit("/dashboard");
+
+    cy.intercept("GET", "http://localhost:3000/pokemon", {
+      fixture: "pokemons.json",
+    });
+    cy.intercept("GET", "http://localhost:3000/pokemon/1", {
+      fixture: "pokemonDetail.json",
+    });
+
+    cy.contains("Pikachu").click();
+    cy.contains("Pikachu");
+    cy.contains("Voltar");
+  });
+
+  it("Voltar para a listagem de 4 pokemons", () => {
+    cy.visit("/dashboard");
+
+    cy.intercept("GET", "http://localhost:3000/pokemon", {
+      fixture: "pokemons.json",
+    });
+    cy.intercept("GET", "http://localhost:3000/pokemon/1", {
+      fixture: "pokemonDetail.json",
+    });
+
+    cy.contains("Pikachu").click();
+    cy.contains("Pikachu");
+    cy.contains("Voltar").click();
+
+    cy.contains("Pikachu");
+    cy.contains("Rotom");
+    cy.contains("Charmander");
+    cy.contains("Geodude");
   });
 });
